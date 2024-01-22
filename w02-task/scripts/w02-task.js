a2a62f66600ef33a52b9d3764c74d9f4c64fb47c
jaredspillari <jaredspillari@gmail.com>
2024-01-21 20:41:19 -0600

    added w02-task

---
+++ w02-task/scripts/w02-task.js
@@ -0,0 +1,31 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = 'Jeffrey Castellanos';
+let currentYear =new Date().getFullYear();
+let  profilePicture = 'images/jeff_face.jpg';
+let favoriteFoodList = ['Pizza', 'Hamburgers', 'Fried Chiken', 'Bacon' ]
+let ul = `<ul>${favoriteFoodList.map(food => `<li>${food}</li>`).join('')}</ul>`
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.getElementsByTagName('img');  
+/* Step 4 - Adding Content */
+nameElement.innerHTML =`<strong>${fullName}</strong>`;
+imageElement[0].setAttribute('src', profilePicture);
+foodElement.innerHTML = ul
+
+
+
+
+
+/* Step 5 - Array */
+
+
+
+
+
+
